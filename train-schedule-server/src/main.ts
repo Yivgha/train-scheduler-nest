c9f2ed5e7f36de97421228183caedf017ae0609a
Yivgha <genieukne@gmail.com>
2024-11-17 18:07:21 +0200

    Simplify cors

--- train-schedule-server/src/main.ts
+++ train-schedule-server/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,18 +8,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
-
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    if (req.method === 'OPTIONS') {
-      res.status(204).send();
-    } else {
-      next();
-    }
   });
 
   await app.listen(3001);
