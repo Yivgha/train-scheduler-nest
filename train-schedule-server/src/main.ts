179ff3ebad7797236b0cafb2abad7f5f3cfc9f0c
Yivgha <genieukne@gmail.com>
2024-11-17 17:30:02 +0200

    add cors options middleware

--- train-schedule-server/src/main.ts
+++ train-schedule-server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -12,6 +14,16 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    if (req.method === 'OPTIONS') {
+      res.status(204).send();
+    } else {
+      next();
+    }
+  });
+
   await app.listen(3001);
 }
+
 bootstrap();
