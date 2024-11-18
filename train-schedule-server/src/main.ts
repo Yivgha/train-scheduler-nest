17b3a1ec1b4580290b4f4f6d8cedd00a08b116b7
Yivgha <genieukne@gmail.com>
2024-11-18 15:05:03 +0200

    Change cors args

--- train-schedule-server/src/main.ts
+++ train-schedule-server/src/main.ts
@@ -7,9 +7,11 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true,
-    preflightContinue: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
+    allowedHeaders:
+      'Content-Type, Authorization, Cache-Control, Origin, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Accept',
   });
 
   await app.listen(3001);
